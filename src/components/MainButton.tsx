92a02427aeee7be43fc51dc7fec5c8c7793d5cb2
Kacper Gajdarski <gajdarski.kacper@gmail.com>
2025-02-27 16:18:58 +0000

    previous push didnt push

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -2,46 +2,50 @@ import { easeInOut, motion } from "framer-motion";
 import { MainButtonProp } from "../types/Universal";
 
 export const MainButton = ({ name }: MainButtonProp) => {
-
   return (
     <motion.a
       className="px-[48px] py-[16px] border text-2xl uppercase cursor-pointer max-w-fit flex flex-col relative overflow-hidden"
       whileHover="hovered"
       initial="initial"
     >
-      <motion.div className="absolute px-[48px] py-[16px] left-0 top-0"
-      variants={{
-        initial: {
-          y: 0,
-        },
-        hovered: {
-          y: "-200%",
-          scale: 0.2,
-          transition: { ease: easeInOut, duration: 0.2}
-        },
-      }}
-    >{name}</motion.div>
+      <motion.div
+        className="absolute px-[48px] py-[16px] left-0 top-0"
+        variants={{
+          initial: {
+            y: 0,
+          },
+          hovered: {
+            y: "-200%",
+            scale: 0.2,
+            transition: { ease: easeInOut, duration: 0.2 },
+          },
+        }}
+      >
+        {name}
+      </motion.div>
       <span className="opacity-[0%]">{name}</span>
-      <motion.div className="absolute bg-(--primary) px-[48px] py-[16px] left-0 top-0 text-(--bg)"
-      variants={{
-        initial: {
-          y: "150%",
-          scale: 1
-        },
-        hovered: {
-          y: 0,
-          scale: 1,
-          transition: { ease: easeInOut, duration: 0.2}
-        },
-      }}>{name}</motion.div>
+      <motion.div
+        className="absolute bg-(--primary) px-[48px] py-[16px] left-0 top-0 text-(--bg)"
+        variants={{
+          initial: {
+            y: "150%",
+            scale: 1,
+          },
+          hovered: {
+            y: 0,
+            scale: 1,
+            transition: { ease: easeInOut, duration: 0.2 },
+          },
+        }}
+      >
+        {name}
+      </motion.div>
     </motion.a>
   );
 };
 
-
-
-
- {/* <motion.span
+{
+  /* <motion.span
         className="absolute "
         variants={{
           initial: {
@@ -54,10 +58,11 @@ export const MainButton = ({ name }: MainButtonProp) => {
         }}
       >
         {name}
-      </motion.span> */}
+      </motion.span> */
+}
 
-
-{/* <motion.span
+{
+  /* <motion.span
         className="absolute"
         variants={{
           initial: {
@@ -71,4 +76,5 @@ export const MainButton = ({ name }: MainButtonProp) => {
         }}
       >
         {name}
-      </motion.span> */}
+      </motion.span> */
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,7 @@ import Discover4 from "../assets/home/discover/discover4.jpg";
 import Discover5 from "../assets/home/discover/discover5.avif";
 import Discover6 from "../assets/home/discover/discover6.jpg";
 import { DiscoverImageProp } from "../types/Home";
+import { easeInOut, motion } from "framer-motion";
 
 const Home = () => {
   return (
@@ -23,7 +24,7 @@ const Home = () => {
         <div>
           <AboutUs />
           <Discover />
-          <Categories/>
+          <Categories />
           <Footer />
         </div>
       </section>
@@ -61,48 +62,107 @@ const AboutUs = () => {
 const Discover = () => {
   return (
     <section className="grid grid-cols-4 gap-[12px] justify-between mt-[128px]">
-      <div
-        className="col-span-2 row-span-2 bg-blue-500 DiscoverImgMain"
-        style={{
-          backgroundImage: `url(${Discover1})`,
-          backgroundPosition: "bottom",
-          backgroundSize: 'cover'
-        }}
-      ></div>
+      <DiscoverImageMain image={Discover1} />
       <div className="col-span-2 row-span 2 text-right uppercase text-2xl">
         <h3>Discover our</h3>
         <h3>unique furniture,</h3>
         <h3>designed for you by you</h3>
       </div>
-      <DiscoverImage image={Discover2}/>
-      <DiscoverImage image={Discover3}/>
+      <DiscoverImage image={Discover2} />
+      <DiscoverImage image={Discover3} />
       <div className="col-span-1 row-span-1 flex flex-col justify-end">
         <MainButton name="Explore more" />
       </div>
-      <DiscoverImage image={Discover4}/>
-      <DiscoverImage image={Discover5}/>
-      <DiscoverImage image={Discover6}/>
+      <DiscoverImage image={Discover4} />
+      <DiscoverImage image={Discover5} />
+      <DiscoverImage image={Discover6} />
     </section>
   );
 };
 
-const DiscoverImage = ({image}: DiscoverImageProp) => {
+const DiscoverImageMain = ({ image }: DiscoverImageProp) => {
   return (
-    <div className="col-span-1 row-span-1 DiscoverImg bg-red-600 self-end"
-    style={{
-      backgroundImage: `url(${image})`,
-      backgroundSize: 'cover'
-    }}>
-      </div>
-  )
-}
+    <motion.a
+      className="col-span-2 row-span-2 cursor-pointer relative overflow-hidden"
+      whileHover="hoveredCard"
+      initial="initial"
+    >
+      <motion.div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          backgroundPosition: 'bottom',
+        aspectRatio: "1/.8",
+        }}
+        variants={{
+          initial: { scale: 1.05 }, 
+          hoveredCard: {
+            scale: 1,
+            transition: { ease: "easeInOut", duration: 0.2 },
+          },
+        }}
+      />
+      <motion.div
+        className="flex items-center justify-between px-[16px] py-[16px] text-xl bg-white border border-(--black20) absolute w-full bottom-0"
+        variants={{
+          initial: {
+            y: "100%",
+          },
+          hoveredCard: {
+            y: 0,
+            transition: { ease: easeInOut, duration: 0.2 },
+          },
+        }}
+      >
+        <h1 className="font-[400]">Product Name</h1>
+        <h2 className="text-base">Category</h2>
+      </motion.div>
+    </motion.a>
+  );
+};
+
+const DiscoverImage = ({ image }: DiscoverImageProp) => {
+  return (
+    <motion.a
+      className="col-span-1 row-span-1 DiscoverImg self-end cursor-pointer relative overflow-hidden aspect-square"
+      whileHover="hoveredCard"
+      initial="initial"
+    >
+      <motion.div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${image})`,
+        }}
+        variants={{
+          initial: { scale: 1.05 }, 
+          hoveredCard: {
+            scale: 1,
+            transition: { ease: "easeInOut", duration: 0.2 },
+          },
+        }}
+      />
+      <motion.div
+        className="flex items-center justify-between px-[16px] py-[16px] text-xl bg-white border border-(--black20) absolute w-full bottom-0"
+        variants={{
+          initial: {
+            y: "100%",
+          },
+          hoveredCard: {
+            y: 0,
+            transition: { ease: easeInOut, duration: 0.2 },
+          },
+        }}
+      >
+        <h1 className="font-[400]">Product Name</h1>
+        <h2 className="text-base">Category</h2>
+      </motion.div>
+    </motion.a>
+  );
+};
 
 const Categories = () => {
-  return (
-<section>
-  
-</section>
-  )
-}
+  return <section></section>;
+};
 
 export default Home;
