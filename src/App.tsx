d25f57fc2fbac2ce69e6f5c0509ee61939bab6a7
Kacper Gajdarski <gajdarski.kacper@gmail.com>
2025-03-18 12:39:00 +0000

    replace Account route with Checkout route in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,8 @@ import Explore from "./pages/Explore.tsx";
 import About from "./pages/About.tsx";
 import ItemPreview from "./pages/ItemPreview.tsx";
 import Cart from "./pages/Cart.tsx";
-<<<<<<< HEAD
 import Account from "./pages/Account.tsx";
-=======
 import Checkout from "./pages/Checkout.tsx";
->>>>>>> c877a14 (add Checkout page and integrate with Cart for order processing)
 import { Lenis } from "@studio-freight/react-lenis";
 import { MdExpandMore } from "react-icons/md";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -27,11 +24,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/item-preview" element={<ItemPreview />} />
           <Route path="/cart" element={<Cart />} />
-<<<<<<< HEAD
           <Route path="/account" element={<Account />} />
-=======
           <Route path="/checkout" element={<Checkout />} />
->>>>>>> c877a14 (add Checkout page and integrate with Cart for order processing)
         </Routes>
       </Lenis>
     </Router>
