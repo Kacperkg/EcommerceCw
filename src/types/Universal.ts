bd809c591e44b378d6aaf9c3f81b8bdcfea85044
Kacper Gajdarski <gajdarski.kacper@gmail.com>
2025-03-19 12:52:01 +0000

    Updates

--- src/types/Universal.ts
+++
@@ -1,3 +0,0 @@
-export interface MainButtonProp {
-    name:string;
-}
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  base: "/EcommerceCw/", // Add this line (your repo name)
-});
